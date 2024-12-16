df25e3ed6192afd1aaf4abb458078c3725caf71c
KanhaiyaKumarShukla <kanhaiyashukla0.01@gmail.com>
2024-12-16 21:00:46 +0530

    Initial commit

--- app/src/main/java/com/example/sih/presentation/broadcast/fragment/news/BlogPostFragment.kt
+++ app/src/main/java/com/example/sih/presentation/broadcast/fragment/news/BlogPostFragment.kt
@@ -1,19 +1,51 @@
 package com.example.sih.presentation.broadcast.fragment.news
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.sih.R
 import com.example.sih.common.basefragment.BaseFragment
 import com.example.sih.databinding.FragmentBlogPostBinding
+import com.example.sih.databinding.FragmentMapBinding
+import java.util.UUID
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 
 
 class BlogPostFragment: BaseFragment<FragmentBlogPostBinding>(FragmentBlogPostBinding::inflate) {
 
+    private lateinit var _binding: FragmentBlogPostBinding
+    private val REQUEST_CODE_IMAGE_PICKER = 1001
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentBlogPostBinding.bind(view)
+
+        _binding.addImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_IMAGE_PICKER)
+        }
+
+        _binding.compileButton.setOnClickListener {
+            val markdownContent = _binding.markdownEditor.text.toString()
+
+            // Navigate to CompilerFragment with NavController
+            val navController = findNavController()
+            /*
+            val action = CurrentFragmentDirections.actionCurrentFragmentToCompilerFragment(markdownContent)
+
+            navController.navigate(action)
+
+             */
+
+        }
 
 
     }
--- app/src/main/java/com/example/sih/presentation/broadcast/fragment/news/NewsFragment.kt
+++ app/src/main/java/com/example/sih/presentation/broadcast/fragment/news/NewsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.sih.R
 import com.example.sih.common.basefragment.BaseFragment
 import com.example.sih.databinding.FragmentNewsBinding
@@ -13,7 +14,9 @@ class NewsFragment: BaseFragment<FragmentNewsBinding>(FragmentNewsBinding::infla
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.addNews.setOnClickListener {
+            findNavController().navigate(R.id.action_newsFragment_to_blogPostFragment)
+        }
 
     }
 }
